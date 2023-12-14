38565f28119e36598a1cee8af9c1ffb560d35784
Jakub Neruda <nerudaj@email.cz>
2023-12-14 11:50:18 +0100

    Create RcTest.cpp

---
+++ src/tests/src/RcTest.cpp
@@ -0,0 +1,28 @@
+#include <catch.hpp>
+
+import Memory;
+
+class Base
+{
+public:
+    virtual ~Base() = default;
+
+public:
+    virtual void foo() = 0;
+};
+
+class Derived : public Base
+{
+public:
+    void foo() override {}
+};
+
+TEST_CASE("[Rc]")
+{
+    SECTION("Can be constructed from derived")
+    {
+        auto derived = mem::Rc<Derived>();
+        auto base = mem::Rc<Base>(derived);
+        base->foo();
+    }
+}
