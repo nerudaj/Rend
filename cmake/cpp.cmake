e4f3066bfbf677b84157c7cce11cd769c86a2882
Jakub Neruda <nerudaj@email.cz>
2024-07-20 12:22:07 +0200

    Compiler flags: Prevent any name shadowing

--- cmake/cpp.cmake
+++ cmake/cpp.cmake
@@ -3,10 +3,10 @@ set ( CMAKE_CXX_STANDARD		   23 )
 set ( CMAKE_CXX_STANDARD_REQUIRED  ON )
 set ( CMAKE_CXX_EXTENSIONS         OFF )
 set ( MY_CXX_COMPILE_FEATURES      cxx_generic_lambdas cxx_range_for cxx_strong_enums )
-set ( CXX_COMPILE_OPTIONS          /W4 /MP /we4265 /we4834 /we4456 /wd4251 /we5205 )
+set ( CXX_COMPILE_OPTIONS          /W4 /MP /we4265 /we4834 /we4456 /we4457 /we4458 /we4459 /wd4251 /we5205 )
 # C4265 - Missing virtual dtor
 # C4834 - Discarding result of [[nodiscard]] function
-# C4456 - Name shadowing
+# C4456-4459 - Name shadowing
 # C5205 - Dtor on iface is not virtual
 
 if (NOT CMAKE_DEBUG_POSTFIX)
