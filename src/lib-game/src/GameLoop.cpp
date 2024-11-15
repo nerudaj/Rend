5f726ca44414ed0906df5c6aaf80c6ef0fbde8e2
Jakub Neruda <nerudaj@email.cz>
2024-11-15 15:29:10 +0100

    cleanup

--- src/lib-game/src/GameLoop.cpp
+++ src/lib-game/src/GameLoop.cpp
@@ -1,14 +1,5 @@
 #include "GameLoop.hpp"
 
-/* DELETEME:
-void GameLoop::update(const float dt, const float realDt, bool skipAudio)
-{
-    aiEngine.update(dt); // FIXME: must happen after everything else
-    updateEngines(dt, realDt);
-    processEvents(skipAudio);
-    gameRulesEngine.deleteMarkedObjects();
-}*/
-
 void GameLoop::renderTo(dgm::Window& window)
 {
     renderingEngine.renderTo(window);
