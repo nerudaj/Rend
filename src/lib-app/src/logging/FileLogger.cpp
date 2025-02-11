5b619c946c760a5c7f955dc34c355dcb0eafa479
Jakub Neruda <nerudaj@email.cz>
2025-02-11 21:23:19 +0100

    Add timestamps to logger

--- src/lib-app/src/logging/FileLogger.cpp
+++ src/lib-app/src/logging/FileLogger.cpp
@@ -6,7 +6,7 @@ FileLogger::FileLogger(const std::filesystem::path& logfilePath)
     : logstream(logfilePath)
 {
     setLogLevel(LogLevel::All);
-    std::println(logstream, "Tick,Severity,Message");
+    std::println(logstream, "Tick,Severity,Message,Time");
 }
 
 static inline std::string replaceAll(
@@ -21,10 +21,29 @@ static inline std::string replaceAll(
 void FileLogger::logImpl(
     size_t tick, LogSeverity severity, const std::string& message)
 {
+    auto now = std::chrono::system_clock::now();
+    auto duration = now.time_since_epoch();
+
+    auto hours = std::chrono::duration_cast<std::chrono::hours>(duration);
+    duration -= hours;
+    auto minutes = std::chrono::duration_cast<std::chrono::minutes>(duration);
+    duration -= minutes;
+    auto seconds = std::chrono::duration_cast<std::chrono::seconds>(duration);
+    duration -= seconds;
+    auto milliseconds =
+        std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+    std::string formatted_time = std::format(
+        "{:02}:{:02}:{:02}.{:03}",
+        hours.count() % 24,
+        minutes.count() % 60,
+        seconds.count() % 60,
+        milliseconds.count());
+
     std::println(
         logstream,
-        "{},{},\"{}\"",
+        "{},{},\"{}\",{}",
         tick,
         severity == LogSeverity::Debug ? "Debug" : "Error",
-        replaceAll(message, "\"", "\"\""));
+        replaceAll(message, "\"", "\"\""),
+        formatted_time);
 }
